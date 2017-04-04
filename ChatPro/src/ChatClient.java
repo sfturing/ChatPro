a23fe7309188bc9e6df2e5148792ed2c919c521c
motionnnnn <motionnnnn@111.164.214.111>
2017-04-04 18:53:45 +0800

    frame

---
+++ ChatPro/src/ChatClient.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class ChatClient extends Frame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ChatClient().launchFrame();
+	}
+	public void launchFrame(){
+		this.setLocation(300, 300);
+		this.setSize(300, 300);
+		this.setVisible(true);
+	}
+
+}
