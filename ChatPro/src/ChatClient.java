b10a4fc1eb0df0dfa1f86bba2336318afee4e581
sfturing <Administrator@C321-040>
2017-04-07 17:27:51 +0800

    not final

--- ChatPro/src/ChatClient.java
+++ ChatPro/src/ChatClient.java
@@ -6,67 +6,79 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ChatClient extends Frame {
-	Socket s=null;
+	Socket s = null;
 	DataOutputStream dos = null;
-	TextField textField=new TextField();
-	TextArea textArea=new TextArea();
-	/*感觉自己仍然需要很多的努力*/
+	DataInputStream dis = null;
+	TextField textField = new TextField();
+	TextArea textArea = new TextArea();
+	String accStr = "";
+	String writeStr = "";
+
+	/* 感觉自己仍然需要很多的努力 */
 	/**
 	 * @param args
 	 */
+
+	// 主方法
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		new ChatClient().launchFrame();
+		try {
+			new ChatClient().launchFrame();
+		} catch (Exception e) {
+			System.out.println("连接服务器失败，请重新连接 .");
+		}
 	}
-	public void launchFrame(){
+
+	// 窗口加载类
+	public void launchFrame() {
 		this.setLocation(300, 300);
 		this.setSize(300, 300);
-		this.add(textField,BorderLayout.SOUTH);
-		this.add(textArea,BorderLayout.NORTH);
+		this.add(textField, BorderLayout.SOUTH);
+		this.add(textArea, BorderLayout.NORTH);
 		this.pack();
 		this.addWindowListener(new WindowAdapter() {
-
 			@Override
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
 				disConnect();
 			}
-			
+
 		});
-		this.textField.addActionListener(new TFListener());
+		textField.addActionListener(new TFListener());
 		this.setVisible(true);
 		connect();
+
 	}
-	public void connect(){
+
+	// 连接方法
+	public void connect() {
 		try {
-			s=new Socket("127.0.0.1",4444);
+			s = new Socket("127.0.0.1", 4444);
+			Server server = new Server(s);
+			new Thread(server).start();
 			System.out.println("Connect!");
-			dos=new DataOutputStream(s.getOutputStream());
+			dis = new DataInputStream(s.getInputStream());
+			dos = new DataOutputStream(s.getOutputStream());
 		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("连接服务器失败，请重新连接 .");
+			System.exit(0);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("连接服务器失败，请重新连接 .");
+			System.exit(0);
 		}
 	}
-	public void sendMessage(String str){
-		try {
-			dos.writeUTF(str);
-			dos.flush();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	public void disConnect(){
+
+	// 断开连接
+	public void disConnect() {
 		try {
 			dos.close();
 			s.close();
@@ -74,19 +86,94 @@ public class ChatClient extends Frame {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	private class TFListener implements ActionListener{
+
+	// 输入监听内部类
+	private class TFListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			String str=textField.getText().trim();
-			sendMessage(str);
-			textArea.setText(str);
-			textField.setText("");
-			
+			// 输入字符
+			writeStr = textField.getText().trim();
+			sendStr(writeStr);
+			// 获取当前时间
+
 		}
-		
+
 	}
 
+	// 线程
+	class Server implements Runnable {
+		private Socket s = null;
+		/*
+		 * private DataInputStream dis = null; private DataOutputStream dos =
+		 * null;
+		 */
+		private boolean isConnect = false;
+
+		Server(Socket s) {
+			this.s = s;
+			try {
+				dis = new DataInputStream(s.getInputStream());
+				dos = new DataOutputStream(s.getOutputStream());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			isConnect = true;
+
+		}
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			while (isConnect) {
+				try {
+					accStr = dis.readUTF();
+					textArea.setText(accStr);
+				} catch (IOException e) {
+					System.out.println("读取中");
+				} finally {
+					try {
+						if (dis == null)
+							dis.close();
+						if (dos == null)
+							dos.close();
+						if (s == null)
+							s.close();
+					} catch (IOException e1) {
+						e1.printStackTrace();
+					}
+				}
+			}
+		}
+
+	}
+
+	// 发送字符
+	public void sendStr(String writeStr) {
+		// 获取当前时间
+		Calendar calendar = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yy/mm/dd hh:mm:ss");
+		// 时间
+		String timeStr = sdf.format(calendar.getTime()) + "\n" + writeStr;
+		accStr = accStr + "\n" + timeStr + "\n";
+		sendMessage(accStr);
+
+		textField.setText("");
+		// System.out.println(accStr);
+	}
+
+	public void sendMessage(String str) {
+		try {
+			dos.writeUTF(str);
+			dos.flush();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
--- ChatPro/src/ChatServer.java
+++ ChatPro/src/ChatServer.java
@@ -9,19 +9,23 @@ import java.net.Socket;
 import java.util.ArrayList;
 
 public class ChatServer {
+	// 判断是否接受客户端连接请求
 	boolean isStarted = false;
 	ServerSocket ss = null;
 	Socket s = null;
+	// 客户端的集合
 	ArrayList<Client> cliens = new ArrayList<Client>();
 
 	/**
 	 * @param args
 	 */
+	// 主方法
 	public static void main(String[] args) {
 		new ChatServer().start();
 
 	}
 
+	// 服务器运行方法
 	public void start() {
 		try {
 			ss = new ServerSocket(4444);
@@ -40,6 +44,7 @@ public class ChatServer {
 				System.out.println("A client connected");
 				Client c = new Client(s);
 				cliens.add(c);
+				// 开启线程接受客户端消息
 				new Thread(c).start();
 
 			} catch (IOException e) {
@@ -48,48 +53,55 @@ public class ChatServer {
 		}
 
 	}
-	
-	
+
+	// 线程
 	class Client implements Runnable {
 		private Socket s = null;
-		private DataInputStream dis = null;	
+		private DataInputStream dis = null;
 		private DataOutputStream dos = null;
 		private boolean isConnect = false;
 
+		// 构造方法
 		public Client(Socket s) {
 			this.s = s;
 			try {
 				dis = new DataInputStream(s.getInputStream());
-				dos=new DataOutputStream(s.getOutputStream());
+				dos = new DataOutputStream(s.getOutputStream());
 				isConnect = true;
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
- 
+
 		public void run() {
 			try {
+				// 接受消息并发送给每一个客户端
 				while (isConnect) {
 					String str = dis.readUTF();
 					System.out.println(str);
-					for(int i=0;i<cliens.size();i++){
+					for (int i = 0; i < cliens.size(); i++) {
 						cliens.get(i).sendMessage(str);
 					}
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				System.out.println("客户端退出.");
 			} finally {
 				try {
-					if(dis!=null) dis.close();
-					if(dos!=null) dos.close();
-					if(s!=null) s.close();
+					if (dis == null)
+						dis.close();
+					if (dos == null)
+						dos.close();
+					if (s == null)
+						s.close();
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
 			}
 
 		}
-		public void sendMessage(String str){
+
+		// 发送消息的请求
+		public void sendMessage(String str) {
 			try {
 				dos.writeUTF(str);
 			} catch (IOException e) {
